e9e5e80edc88c2a5f61adfff6e7b9ba1222946fc
mmlocc <111124140+mmlocc@users.noreply.github.com>
2023-07-13 18:24:48 +0900

    Update second.dart
    error 수정

--- lib/second.dart
+++ lib/second.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'main.dart';
 
-class Second extends StatelessWidget {
+class Second extends StatelessWidget {
 const Second({Key? key}) : super(key: key);
 
 @override
